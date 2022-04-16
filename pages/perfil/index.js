ae54b669f355c5e67506f7c4fc63bd46189b562e
Jaime de Greiff <alegreiff@gmail.com>
2022-04-15 21:11:36 -0500

    perfil variables

--- pages/perfil/index.js
+++ pages/perfil/index.js
@@ -5,14 +5,10 @@ export default function PerfilPage() {
   const { user, perfil } = useStore();
   //console.log({ datos });
 
-  if (perfil) {
-    return (
-      <Layout titulo={`Perfil de ${user.name}`}>
-        <h2>Perfil de usuario</h2>
-        <span> {perfil?.hincha} </span>
-      </Layout>
-    );
-  } else {
-    return <h3>NO USER</h3>;
-  }
+  return (
+    <Layout titulo={`Nuestra Polla - Perfil de ${user?.name}`}>
+      <h2>Perfil de usuario</h2>
+      <span> {perfil?.hincha} </span>
+    </Layout>
+  );
 }

098fe1774a0cc080c6eabc46f56626014fe713ea
Jaime de Greiff <alegreiff@gmail.com>
2022-04-15 20:59:18 -0500

    secreto

--- pages/perfil/_middleware.js
+++ pages/perfil/_middleware.js
@@ -2,7 +2,7 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req, ev) {
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const session = await getToken({ req, secret: 'jaja' });
   console.log('Desde MiddleWare', session);
 
   if (!session) {

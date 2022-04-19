e8599e8a045a5d4ca1d16253352eb836244d6695
Jaime de Greiff <alegreiff@gmail.com>
2022-04-19 14:19:31 -0500

    midleguares

---
+++ pages/perfil/_middleware.js
@@ -0,0 +1,17 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req, ev) {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log('Desde MiddleWare', session);
+
+  if (!session) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/pollero/signin';
+    console.log('URL', url);
+    return NextResponse.redirect(url);
+  } else {
+    console.log('hurrah');
+    return NextResponse.next();
+  }
+}

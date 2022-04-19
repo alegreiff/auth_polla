c25ccfc28a245d90f77ff79fb3f63ab26af46681
Jaime de Greiff <alegreiff@gmail.com>
2022-04-19 14:49:30 -0500

    kirie

--- pages/perfil/_middleware.js
+++ pages/perfil/_middleware.js
@@ -2,17 +2,22 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req, ev) {
-  const secret = process.env.NEXTAUTH_SECRET;
-  const session = await getToken(req, secret);
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    encryption: true,
+  });
   console.log('Desde MiddleWare', session, process.env.NEXTAUTH_SECRET);
 
   if (!session) {
+    console.log('JSON Web Token', JSON.stringify(session, null, 2));
+
     const url = req.nextUrl.clone();
     url.pathname = '/pollero/signin';
     console.log('URL', url);
     return NextResponse.redirect(url);
   } else {
-    console.log('hurrah');
+    //console.log('hurrah');
     return NextResponse.next();
   }
 }

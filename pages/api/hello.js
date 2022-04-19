c543043cf58ef86438451b0686ff5e9daa62c761
Jaime de Greiff <alegreiff@gmail.com>
2022-04-19 15:49:04 -0500

    token 'e

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     res.status(200).json({ token });
   } else {
     // Not Signed in
-    res.status(401);
+    res.status(200).json({ message: 'No token' });
   }
   res.end();
   //res.status(200).json({ name: 'John Doe' })
